8fcef6460db97679b2f55d7d1d9a28ff1340eb4f
lizhanwen <lizhanwen@bytedance.com>
2023-12-23 14:13:13 +0800

    feat: init

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
-    plugins: [react()],
-    server: {
-        host: '0.0.0.0',
-    }
-})
+  base: "./",
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+  },
+  build: {
+    outDir: "output",
+  },
+});
